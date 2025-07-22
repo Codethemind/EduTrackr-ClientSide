d4596d29acefc13fa6ca1b77ca97edf7389ff719
Mohammmed shahid <mhdshahid88@gmail.com>
2025-07-22 21:51:14 +0530

    main.tsx bug fixed

--- src/main.tsx
+++ src/main.tsx
@@ -6,12 +6,15 @@ import  store  from './redux/store.js';
 import App from './app.tsx';
 import './index.css';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
-  </StrictMode>
-);
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  createRoot(rootElement).render(
+    <StrictMode>
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
+    </StrictMode>
+  );
+}
