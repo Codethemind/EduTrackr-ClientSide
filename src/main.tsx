809cb6a88ebf659317a76b650f027fcf8c037b1f
Mohammmed shahid <mhdshahid88@gmail.com>
2025-07-25 17:16:06 +0530

    bug fixed in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import  store  from './redux/store.js';
-import App from './app.tsx';
+import App from './App.tsx';
 import './index.css';
 
 const rootElement = document.getElementById('root');
